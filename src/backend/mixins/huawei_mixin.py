763fa56584fe470abe52db405ba6cc07b94f4308
Paul Urdaneta <urdaneta.villalobos.paul@gmail.com>
2026-04-16 11:26:47 -0600

    fix(backend): obtención estado de USB
    - Se añadió una espera de 1 segundo para asegurar que cargue la sección _USB Application_ y obtener bien el estado.

--- src/backend/mixins/huawei_mixin.py
+++ src/backend/mixins/huawei_mixin.py
@@ -1009,7 +1009,8 @@ class HuaweiMixin:
             wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, "menuIframe")))
             print("[SELENIUM] iframe 'menuIframe' disponible para USB Application.")
 
-            # 5) Esperar el select de USB
+            # 5) Esperar el select de USB (dar tiempo a que cargue el contenido dentro del iframe)
+            time.sleep(1)
             wait.until(EC.presence_of_element_located((By.ID, "SrvClDevType")))
             print("[SELENIUM] Select USB 'SrvClDevType' encontrado en página USB Application.")
 
